96e7c3f8593103ad4e2999009b98d7855d2f5c68
RahulPatnaik <rpatnaik2005@gmail.com>
2025-04-07 17:08:28 +0530

    Initate

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import SideNav  from "../ui/dashboard/sidenav";
+
+export default function Layout({children}: {children: React.ReactNode}){
+    return (
+        <div className="flex h-screen">
+            <SideNav/>
+            <main className="flex-1 p-4 overflow-y-auto">
+                {children}
+            </main>
+        </div>
+    );
+}
